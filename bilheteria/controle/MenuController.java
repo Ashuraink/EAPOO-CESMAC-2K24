8693349bcfc0c2701bc7347539a8d4fe6b8c7b49
Administravo Hosp Maceio <admmco@hapvida.com.br>
2023-11-28 13:27:20 -0300

    Junção da função comprarIngresso e comprarIngressoVIP em apenas uma

--- bilheteria/controle/MenuController.java
+++ bilheteria/controle/MenuController.java
@@ -7,7 +7,6 @@ import bilheteria.ingresso.Ingressos;
 import bilheteria.ingresso.IngressosVip;
 import bilheteria.modelo.Filme;
 
-
 public class MenuController {
 
     static Scanner scanner = new Scanner(System.in);
@@ -35,7 +34,6 @@ public class MenuController {
                 new Filme("One piece - Gold", "Miyamoto Hiroaki", "Situado em Gran Tesoro, um país independente fretado pelo Governo Mundial, \nlar da maior cidade de entretenimento do mundo, onde piratas, fuzileiros navais e milionários conhecidos em todo o mundo \nse reúnem em um santuário absoluto que nem o Governo Mundial pode tocar.", "Animação/Aventura", 130, false),
                 new Filme("Avatar 2", "James Cameron", "Continuação do épico Avatar, o filme traz novas aventuras no planeta Pandora em 3D.", "Aventura/Ficção Científica", 150, true)
         };
-
         for (int i = 0; i < filmes.length; i++) {
             Filme filmeAtual = filmes[i];
             System.out.println((i + 1) + ". " + filmeAtual.getNome());
@@ -46,89 +44,62 @@ public class MenuController {
             System.out.println("   3D: " + (filmeAtual.isFilme3D() ? "Sim" : "Não"));
             System.out.println();
         }
-    
+
         System.out.print("Escolha o número do filme desejado: ");
         int escolhaFilme = scanner.nextInt();
         if (escolhaFilme < 1 || escolhaFilme > filmes.length) {
             System.out.println("Número de filme incorreto. Tente novamente.");
             return;
         }
-    
+
         Filme filmeEscolhido = filmes[escolhaFilme - 1];
-    
+
         System.out.print("Deseja comprar um ingresso VIP para o filme? (sim/não): ");
         boolean escolhaIngressoVIP = scanner.next().equalsIgnoreCase("sim");
-    
-        if (escolhaIngressoVIP) {
-            comprarIngressoVIP(filmeEscolhido);
-        } else {
-            comprarIngresso(filmeEscolhido);
-        }
-    }
 
-    private static void comprarIngresso(Filme filmeEscolhido) {
         System.out.println("Os horários disponíveis são:");
         String[] nossosHorarios = {"17:00", "18:50", "20:00"};
-    
+
         for (int i = 0; i < nossosHorarios.length; i++) {
             System.out.println((i + 1) + ". " + nossosHorarios[i]);
         }
-    
+
         System.out.print("Escolha o horário da sessão desejada: ");
         int escolhaHorario = scanner.nextInt();
         if (escolhaHorario < 1 || escolhaHorario > nossosHorarios.length) {
             System.out.println("Número de horário incorreto. Tente novamente.");
             return;
         }
-    
+
         System.out.print("Quantidade de bilhetes inteiros desejada: ");
         int quantidadeInteira = scanner.nextInt();
         System.out.print("Quantidade de meia-entrada desejada: ");
         int quantidadeMeia = scanner.nextInt();
-    
-        Ingressos ingresso = new IngressoComum(filmeEscolhido, nossosHorarios[escolhaHorario - 1],
-            quantidadeInteira, quantidadeMeia);
-    
-        System.out.println("\nResumo da compra para Ingresso Comum:");
-        System.out.println("Ingressos para o filme: " + ingresso.getFilme().getNome());
-        System.out.println("Sessão: " + ingresso.getHorario());
-        System.out.println("Ingressos Inteiros: " + ingresso.getQuantidadeInteira());
-        System.out.println("Ingressos Meia-entrada: " + ingresso.getQuantidadeMeia());
+
+        Ingressos ingresso;
+        if (escolhaIngressoVIP) {
+            ingresso = new IngressosVip(filmeEscolhido, nossosHorarios[escolhaHorario - 1],
+                    quantidadeInteira, quantidadeMeia);
+
+            System.out.println("\nResumo da compra para Ingresso VIP:");
+            System.out.println("Ingressos VIP para o filme: " + ingresso.getFilme().getNome());
+            System.out.println("Sessão: " + ingresso.getHorario());
+            System.out.println("Ingressos Inteiros VIP: " + ((IngressosVip) ingresso).getQuantidadeInteira());
+            System.out.println("Ingressos Meia-entrada VIP: " + ((IngressosVip) ingresso).getQuantidadeMeia());
+        } else {
+            ingresso = new IngressoComum(filmeEscolhido, nossosHorarios[escolhaHorario - 1],
+                    quantidadeInteira, quantidadeMeia);
+
+            System.out.println("\nResumo da compra para Ingresso Comum:");
+            System.out.println("Ingressos para o filme: " + ingresso.getFilme().getNome());
+            System.out.println("Sessão: " + ingresso.getHorario());
+            System.out.println("Ingressos Inteiros: " + ingresso.getQuantidadeInteira());
+            System.out.println("Ingressos Meia-entrada: " + ingresso.getQuantidadeMeia());
+        }
+
         System.out.printf("Valor total a ser pago: R$%.2f\n", ingresso.calcularValorTotal());
         System.out.println("Bom filme!");
         ingresso.acessoLanchonete();
-    }
-
-    private static void comprarIngressoVIP(Filme filmeEscolhido) {
-        System.out.println("Os horários disponíveis são:");
-        String[] nossosHorarios = {"17:00", "18:50", "20:00"};
-        for (int i = 0; i < nossosHorarios.length; i++) {
-            System.out.println((i + 1) + ". " + nossosHorarios[i]);
-        }
-
-        System.out.print("Escolha o horário da sessão desejada: ");
-        int escolhaHorario = scanner.nextInt();
-        if (escolhaHorario < 1 || escolhaHorario > nossosHorarios.length) {
-            System.out.println("Número de horário incorreto. Tente novamente.");
-            return;
-        }
-
-        System.out.print("Quantidade de bilhetes inteiros desejada: ");
-    int quantidadeInteira = scanner.nextInt();
-    System.out.print("Quantidade de meia-entrada desejada: ");
-    int quantidadeMeia = scanner.nextInt();
-
-        IngressosVip ingressoVIP = new IngressosVip(filmeEscolhido, nossosHorarios[escolhaHorario - 1],
-                quantidadeInteira, quantidadeMeia);
-
-            System.out.println("\nResumo da compra para Ingresso VIP:");
-            System.out.println("Ingressos VIP para o filme: " + ingressoVIP.getFilme().getNome());
-            System.out.println("Sessão: " + ingressoVIP.getHorario());
-            System.out.println("Ingressos Inteiros VIP: " + ingressoVIP.getQuantidadeInteira());
-            System.out.println("Ingressos Meia-entrada VIP: " + ingressoVIP.getQuantidadeMeia());
-            System.out.printf("Valor total a ser pago: R$%.2f\n", ingressoVIP.calcularValorTotal());
-            System.out.println("Bom filme!");
-            ingressoVIP.acessoLanchonete();
     }
 
     private static String saudacao() {
